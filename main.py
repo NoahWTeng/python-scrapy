91ab13f321ca521a9b72b88ab4cec035c51e2c85
Noah Wang Teng <NoahRiseUp@Noahs-MacBook-Pro.local>
2020-04-14 19:25:35 +0200

    first commit

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from selenium import webdriver
 from time import sleep
 from secrets import account,pw,after_login_url, url
 
-class OgameBot: 
-    def __init__(self, username, pw):
+class OgameBot(): 
+    def __init__(self):
         self.driver = webdriver.Chrome('./chromedriver')
-        self.username = username
         self.driver.get(url)
         self.driver.find_element_by_xpath('//a[contains(text(), "x")]').click()
         sleep(2)
         self.elem = self.driver.find_element_by_xpath('//div[@id="loginRegisterTabs"]/ul/li').click()
-        self.driver.find_element_by_xpath('//input[@name="email"]').send_keys(self.username)
+        self.driver.find_element_by_xpath('//input[@name="email"]').send_keys(account)
         self.driver.find_element_by_xpath('//input[@name="password"]').send_keys(pw)
         sleep(3)
         self.driver.find_element_by_xpath('//button[@type="submit"]').click()
@@ -19,12 +18,23 @@ class OgameBot:
         sleep(2)
         self.driver.find_element_by_xpath('//button[@type="button"]/span[contains(text(), "Play")]').click()    
         sleep(4)
-        self.driver.switch_to.window(self.driver.window_handles[1])
-        self.driver.get(after_login_url)
         self.driver.close()
-        self.driver.switch_to.window(self.driver.window_handles[0] )
-        sleep(5)
-
-OgameBot(account,pw)
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        self.metal = self.driver.find_element_by_id('resources_metal').get_attribute('innerHTML')
+        self.crystal = self.driver.find_element_by_id('resources_crystal').get_attribute('innerHTML')
+        self.deuterium = self.driver.find_element_by_id('resources_deuterium').get_attribute('innerHTML')
+        self.energy = self.driver.find_element_by_id('resources_energy').get_attribute('innerHTML')
+        
+        
+        
+    def go_to_resource(self):
+        self.driver.find_element_by_xpath('//span[contains(text(),"Recursos")]').click()
+        print(self.metal)
+        print(self.crystal)
+        print(self.deuterium)
+        print(self.energy)
+        
+                
+mybot = OgameBot()
 
 
